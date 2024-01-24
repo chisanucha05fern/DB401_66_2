08331cea76964a3e9f034538067d007fc9dc65f4
chisanucha05fern <65111051@dpu.ac.th>
2024-01-24 12:09:02 +0700

    MyApp

---
+++ shades_of_my_life/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int shade = 15977715;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: GestureDetector(
+          onVerticalDragUpdate: (details) {
+            setState(() {
+              double height = MediaQuery.of(context).size.height -
+                  MediaQuery.of(context).padding.top;
+              shade = (details.localPosition.dy / height * 16777215).round();
+              if (shade < 0) {
+                shade = 0;
+              }
+              if (shade > 16777215) {
+                shade = 16777215;
+              }
+            });
+          },
+          onTapDown: (details) {
+            setState(() {
+              double height = MediaQuery.of(context).size.height -
+                  MediaQuery.of(context).padding.top;
+              shade = (details.localPosition.dy / height * 16777215).round();
+            });
+            // print(details.localPosition.dy);
+            // print(MediaQuery.of(context).size.height);
+            // print(MediaQuery.of(context).padding.top);
+          },
+        )),
+        backgroundColor: Color(0xFF000000 + shade),
+      ),
+    );
+  }
+}
